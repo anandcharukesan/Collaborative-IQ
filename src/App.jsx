fad3d476e2e9315572d5860facbc81cd703d0ef9
anandcharukesan <anandcharukesan004@gmail.com>
2024-11-30 11:49:12 +0530

    dashboard update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginComponent from "./pages/auth/loginComponent";
 import RegistrationComponent from "./pages/auth/RegistrationComponent"; // Ensure this path is correct
+import DashboardComponent from "./pages/dashboard/dashboardComponent"; // Ensure this path is correct"; // Ensure this path is correct
 import "./index.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/auth" element={<LoginComponent />} />
           <Route path="/register" element={<RegistrationComponent />} />
+          <Route path="/dashboard" element={<DashboardComponent />} />
           {/* Add more routes as needed */}
         </Routes>
       </div>
