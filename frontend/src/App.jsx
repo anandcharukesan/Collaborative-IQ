61cc8700813b6ae5288fdc5f60ace20ef25cdf1b
anandcharukesan <anandcharukesan004@gmail.com>
2024-11-30 15:51:48 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginComponent from "./pages/auth/loginComponent";
+import RegistrationComponent from "./pages/auth/RegistrationComponent"; // Ensure this path is correct
+import DashboardComponent from "./pages/dashboard/DashboardComponent"; // Ensure this path is correct"; // Ensure this path is correct
+import "./index.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <Routes>
+          <Route path="/auth" element={<LoginComponent />} />
+          <Route path="/register" element={<RegistrationComponent />} />
+          <Route path="/dashboard" element={<DashboardComponent />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
