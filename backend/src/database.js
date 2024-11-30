ec62c0146ff0e798bc96665fe16b962d3c6443c3
anandcharukesan <anandcharukesan004@gmail.com>
2024-11-30 23:01:10 +0530

    auth part

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,15 +1,6 @@
 const mysql = require('mysql2'); // Use `mysql2` for better SSL support
 
-const db = mysql.createConnection({
-    host: 'mysql-83d1e8d-iqcollaborative-ea09.e.aivencloud.com',       // Example: 'mysql-12345.aivencloud.com'
-    user: 'avnadmin',         // Provided by Aiven
-    password: 'AVNS_7fXFdQalrC_FpC7bLrD',     // Provided by Aiven
-    database: 'collaborativeIq',     // Your database name
-    port: 21505,                   // Default port for Aiven MySQL
-    ssl: {
-        rejectUnauthorized: false, // Important for Aiven databases
-    },
-});
+
 
 db.connect((err) => {
     if (err) {
