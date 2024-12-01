647d788426729f676be22e7e6c511d6de39c74a1
Anand Charukesan K <130738098+anandcharukesan@users.noreply.github.com>
2024-12-01 21:46:14 +0530

    Update database.js

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,16 +1,5 @@
 import mysql from "mysql2";
-/////////Credntial here
-
-const db = mysql.createConnection({
-    host: 'mysql-83d1e8d-iqcollaborative-ea09.e.aivencloud.com',
-    user: 'avnadmin',
-    password: 'AVNS_7fXFdQalrC_FpC7bLrD',     
-    database: 'collaborativeIq',
-    port: 21505,
-    ssl: {
-        rejectUnauthorized: false, // Important for Aiven databases
-    },
-});
+/////////Credential here
 
 
 db.connect((err) => {
